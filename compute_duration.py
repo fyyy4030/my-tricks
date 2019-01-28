fd7a99c72c1f1afcd0fc0a2a344fbeab36883f65
wangzihao <wzhy1234@qq.com>
2019-01-28 16:07:57 +0000

    xixi

---
+++ compute_duration.py
@@ -0,0 +1,25 @@
+import os
+import datetime
+import sys
+import argparse
+from moviepy.editor import VideoFileClip
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Compute Total Time of a Series of Videos')
+    parser.add_argument("--path", metavar="PATH", default=".",
+                        help="the root path of the videos(default: .)")
+    parser.add_argument("--type", metavar="TYPE", default=".mkv",
+                        help="the type of the videos(default: .mkv)")
+    args = parser.parse_args()
+    filelist = []
+    for a, b, c in os.walk(args.path):
+        for name in c:
+            fname = os.path.join(a, name)
+            if fname.endswith(args.type):
+                filelist.append(fname)
+    ftime = 0.0
+    for item in filelist:
+        clip = VideoFileClip(item)
+        ftime += clip.duration
+    print(str(datetime.timedelta(seconds=ftime)))
